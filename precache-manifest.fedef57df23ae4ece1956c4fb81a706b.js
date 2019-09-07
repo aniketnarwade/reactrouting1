8ebfdab4120a8aac2dd19c76c10244115901995b
aniketnarwade <50009671+aniketnarwade@users.noreply.github.com>
2019-09-07 12:31:52 +0530

    new

---
+++ precache-manifest.fedef57df23ae4ece1956c4fb81a706b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f846d8022329f8adf0d0f68214fc07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83e42b711aa5566cd597",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "1873d012ceb1f6a3b7b7",
+    "url": "/static/js/2.5bc0451a.chunk.js"
+  },
+  {
+    "revision": "83e42b711aa5566cd597",
+    "url": "/static/js/main.6ff921f8.chunk.js"
+  },
+  {
+    "revision": "474f79572f729dca25cd",
+    "url": "/static/js/runtime~main.b56cd54a.js"
+  }
+]);
